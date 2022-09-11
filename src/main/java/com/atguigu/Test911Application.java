016c6f450d27277bd566ab1d0cd6c0633a115dc6
Jesse <justfortest@gmail.com>
2022-09-11 09:40:54 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/Test911Application.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test911Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test911Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/atguigu/Test911ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test911ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
